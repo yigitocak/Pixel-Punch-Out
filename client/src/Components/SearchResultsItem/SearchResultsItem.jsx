5bc32cf5cc132ee5ceaa600fc35f4840fd805428
yigit ocak <yigitockk@gmail.com>
2024-05-12 17:21:39 -0400

    Implemented a user search functionality. Added a search bar to the site header allowing users to find other players by username.

---
+++ client/src/Components/SearchResultsItem/SearchResultsItem.jsx
@@ -0,0 +1,29 @@
+import "./SearchResultsItem.scss";
+import arrow from "../../assets/images/arrow.svg";
+import { useNavigate } from "react-router-dom";
+
+export const SearchResultsItem = ({ user }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/profiles/${user.username}`);
+  };
+  return (
+    <li className="result__item" key={user.id}>
+      <img
+        className="result__picture"
+        src={user.photoUrl}
+        alt="user profile picture"
+      />
+      <span className="result__username">{user.username}</span>
+      <span className="result__wins">Wins: {user.wins}</span>
+      <span className="result__losses">Losses: {user.losses}</span>
+      <img
+        className="result__link"
+        src={arrow}
+        alt="link image"
+        onClick={handleClick}
+      />
+    </li>
+  );
+};
