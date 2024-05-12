276a35b5c018dac53b0056ca0fd1a1e2ae62bb03
yigit ocak <yigitockk@gmail.com>
2024-05-12 17:22:23 -0400

    Add "Remember Me" functionality to LoginForm. Updated LoginForm component to include a "Remember Me" checkbox that is now being passed to the request body in axios post for login.

--- client/src/Components/LoginForm/LoginForm.jsx
+++ client/src/Components/LoginForm/LoginForm.jsx
@@ -1,92 +1,79 @@
-import './LoginForm.scss'
-import {NavLink} from "react-router-dom";
-import {useState} from "react";
+import "./LoginForm.scss";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import axios from "axios";
-import {BASE_URL} from "../../utils/utils";
+import { BASE_URL } from "../../utils/utils";
 
-export const LoginForm = ({setIsLoggedIn, renderUsername}) => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [error, setError] = useState("");
+export const LoginForm = ({ setIsLoggedIn, renderUsername }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+  const [error, setError] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        setError("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
 
-        if(!email || !password) {
-           return setError("Please fill out the fields!")
-        }
-
-        try {
-            const response = await axios.post(`${BASE_URL}auth/login`,
-                {email, password});
-            localStorage.setItem('authToken', response.data.token)
-            setIsLoggedIn(true)
-            renderUsername(response.data.username)
-
-        } catch (error) {
-            console.error("Signup failed: ", error);
-            if(error.response.status) setError("Sorry, your email or password was incorrect.")
-            else setError("Login failed. Please try again later.");
-        }
+    if (!email || !password) {
+      return setError("Please fill out the fields!");
     }
 
-    return (
-        <form
-            className="login__form"
-            onSubmit={handleSubmit}
-        >
-            <label
-                className="login__label"
-            >
-                Email
-                <input
-                    placeholder="Enter your email"
-                    type="text"
-                    className="login__input"
-                    autoComplete="email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                />
-            </label>
+    try {
+      const response = await axios.post(`${BASE_URL}auth/login`, {
+        email,
+        password,
+        rememberMe,
+      });
+      localStorage.setItem("authToken", response.data.token);
+      setIsLoggedIn(true);
+      renderUsername(response.data.username);
+    } catch (error) {
+      console.error("Signup failed: ", error);
+      if (error.response.status)
+        setError("Sorry, your email or password was incorrect.");
+      else setError("Login failed. Please try again later.");
+    }
+  };
 
-            <label
-                className="login__label"
-            >
-                Password
-                <input
-                    placeholder="Enter your password"
-                    type="password"
-                    className="login__input"
-                    autoComplete="current-password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                />
-            </label>
-            <label
-                className="login__remember-label"
-            >
-                <input
-                    type="checkbox"
-                    className="login__checkbox"
-                />
-                <span
-                    className="login__remember"
-                >Remember me
-                    </span>
-            </label>
-            <button
-                className="login__button"
-            >
-                Login
-            </button>
-            {error && <div className="login__error">{error}</div>}
-            <NavLink
-                className="login__signup"
-                to="/signup"
-            >
-                Sign Up now!
-            </NavLink>
-        </form>
-    )
-}
+  return (
+    <form className="login__form" onSubmit={handleSubmit}>
+      <label className="login__label">
+        Email
+        <input
+          placeholder="Enter your email"
+          type="text"
+          className="login__input"
+          autoComplete="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+
+      <label className="login__label">
+        Password
+        <input
+          placeholder="Enter your password"
+          type="password"
+          className="login__input"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label className="login__remember-label">
+        <input
+          type="checkbox"
+          className="login__checkbox"
+          checked={rememberMe}
+          onChange={(e) => setRememberMe(e.target.checked)}
+        />
+        <span className="login__remember">Remember me</span>
+      </label>
+      <button className="login__button">Login</button>
+      {error && <div className="login__error">{error}</div>}
+      <NavLink className="login__signup" to="/signup">
+        Sign Up now!
+      </NavLink>
+    </form>
+  );
+};
--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,20 +1,29 @@
-import './LoginPage.scss'
+import "./LoginPage.scss";
 import { LoginForm } from "../../Components/LoginForm/LoginForm";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
-export const LoginPage = ({isLoggedIn, setIsLoggedIn, username, renderUsername}) => {
-    const navigate = useNavigate();
+export const LoginPage = ({
+  isLoggedIn,
+  setIsLoggedIn,
+  username,
+  renderUsername,
+  setNumber,
+}) => {
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (isLoggedIn) {
-            navigate(`/profiles/${username}`);
-        }
-    }, [isLoggedIn, username, navigate]); // Dependency array ensures this only runs when these values change
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate(`/profiles/${username}`);
+    }
+  }, [isLoggedIn, username, navigate]); // Dependency array ensures this only runs when these values change
 
-    return (
-        <section className="login">
-            <LoginForm setIsLoggedIn={setIsLoggedIn} renderUsername={renderUsername}/>
-        </section>
-    );
+  return (
+    <section className="login">
+      <LoginForm
+        setIsLoggedIn={setIsLoggedIn}
+        renderUsername={renderUsername}
+      />
+    </section>
+  );
 };
