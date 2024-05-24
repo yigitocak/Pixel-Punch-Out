dbafd389f1d5fee026ea3a6e7066d368a5faa7ce
yigit ocak <yigitockk@gmail.com>
2024-05-24 14:24:49 -0400

    Refactor leaderboard UI and add ranking effects
    Updated the Leaderboard UI for improved readability and aesthetic appeal. Introduced special effects for display of the top three ranked users, providing a visual identifier and enhancing user experience.

--- src/Components/LeaderboardItem/LeaderboardItem.jsx
+++ src/Components/LeaderboardItem/LeaderboardItem.jsx
@@ -1,39 +1,38 @@
-import "./LeaderboardItem.scss"
-import {NavLink} from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./LeaderboardItem.scss";
 
-export const LeaderboardItem = ({wins,losses,username, winrate, id}) => {
-    return (
-        <>
-            <li
-                className="leaderboard__item"
-            >
-            <span
-                className="leaderboard__info leaderboard__username"
-            >
-                <NavLink
-                    className="leaderboard__username"
-                    to={`/profiles/${id}`}
-                >
-                    {username}
-                </NavLink>
-            </span>
-                <span
-                    className="leaderboard__total"
-                >
-            {wins}
-            </span>
-                <span
-                    className="leaderboard__total"
-                >
-            {losses}
-            </span>
-            <span
-                    className="leaderboard__info"
-                >
-            {winrate}
-            </span>
-            </li>
-            <hr/>
-        </>
-    )
-}
+export const LeaderboardItem = ({
+  wins,
+  losses,
+  username,
+  winrate,
+  id,
+  index,
+}) => {
+  const getClassForRank = (rank) => {
+    if (rank === 0) return "golden-effect";
+    if (rank === 1) return "silver-effect";
+    if (rank === 2) return "bronze-effect";
+    return "";
+  };
+
+  const rankClass = getClassForRank(index);
+
+  return (
+    <>
+      <li className={`leaderboard__item ${rankClass}`}>
+        <span className="leaderboard__index">{index + 1}. </span>
+        <span className="leaderboard__info leaderboard__username">
+          <NavLink className="leaderboard__username" to={`/profiles/${id}`}>
+            {username}
+          </NavLink>
+        </span>
+        <span className="leaderboard__total">{wins}</span>
+        <span className="leaderboard__total">{losses}</span>
+        <span className="leaderboard__info">{winrate}</span>
+      </li>
+      <hr />
+    </>
+  );
+};
